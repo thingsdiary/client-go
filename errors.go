93feca7314dc6964479d8ff97e329df47b6b59c0
Mikhail Kalinin <mike@thingsdiary.io>
2025-10-21 23:01:01 +0400

    Handle case when api returns 409 status code

--- errors.go
+++ errors.go
@@ -3,13 +3,14 @@ package client
 import "errors"
 
 var (
-	ErrUnauthorized       = errors.New("unauthorized")
-	ErrForbidden          = errors.New("forbidden: insufficient permissions to perform action")
-	ErrInvalidCredentials = errors.New("invalid credentials")
-	ErrInvalidChallenge   = errors.New("invalid challenge")
-	ErrDiaryNotFound      = errors.New("diary not found")
-	ErrEntryNotFound      = errors.New("entry not found")
-	ErrTopicNotFound      = errors.New("topic not found")
-	ErrTemplateNotFound   = errors.New("template not found")
-	ErrDiaryLimitExceeded = errors.New("diary limit exceeded")
+	ErrUnauthorized         = errors.New("unauthorized")
+	ErrForbidden            = errors.New("forbidden: insufficient permissions to perform action")
+	ErrInvalidCredentials   = errors.New("invalid credentials")
+	ErrAccountAlreadyExists = errors.New("account already exists")
+	ErrInvalidChallenge     = errors.New("invalid challenge")
+	ErrDiaryNotFound        = errors.New("diary not found")
+	ErrEntryNotFound        = errors.New("entry not found")
+	ErrTopicNotFound        = errors.New("topic not found")
+	ErrTemplateNotFound     = errors.New("template not found")
+	ErrDiaryLimitExceeded   = errors.New("diary limit exceeded")
 )
